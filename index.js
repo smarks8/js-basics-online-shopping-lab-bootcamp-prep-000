04cfafa4c1c3b8fe5c32ed0679100eeb7c61d8c3
stacey <staceyamarks@gmail.com>
2017-12-28 02:44:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ function removeFromCart(item) {
       cart.splice(i,1)
       return cart;
     }
-    console.log('')
+    console.log('That item is not in your cart.')
+    return cart;
   }
 }
 
