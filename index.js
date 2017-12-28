3d81cfe6c978d2844d3e3b056d621c140d6e150c
stacey <staceyamarks@gmail.com>
2017-12-28 02:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,5 +73,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}`)
   }
 }
