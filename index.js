1e85dcd8d54d14aaad3940ec777a63a555146682
stacey <staceyamarks@gmail.com>
2017-12-28 02:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,15 +62,13 @@ function total() {
 function removeFromCart(item) {
   
   for(var i =0; i<cart.length;i++){
-    var currentObj = cart[i]
-    if(currentObj.hasOwnProperty(item)){
+    if(cart[i].hasOwnProperty(item)){
       cart.splice(i,1)
       return cart;
-    } else {
-      console.log(`That item is not in your cart.`)
-      return cart;
-    }
+    } 
   }
+  console.log('That item is not in your cart.')
+  return cart
 }
 
 function placeOrder(cardNumber) {
